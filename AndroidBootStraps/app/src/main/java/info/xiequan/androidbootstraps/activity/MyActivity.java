bd1ef1b9931898f85b769b036ac4154f59715b64
spark <xiequan@ganguo.hk>
2014-08-28 04:45:43 +0800

    增加adomb测试广告

--- AndroidBootStraps/app/src/main/java/info/xiequan/androidbootstraps/activity/MyActivity.java
+++ AndroidBootStraps/app/src/main/java/info/xiequan/androidbootstraps/activity/MyActivity.java
@@ -1,28 +1,36 @@
 package info.xiequan.androidbootstraps.activity;
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 import info.xiequan.androidbootstraps.R;
 import info.xiequan.androidbootstraps.base.BaseActivity;
 
 
 public class MyActivity extends BaseActivity {
+    private AdView adView;
+    private static final String AD_UNIT_ID = "ca-app-pub-4706145830093249/3114135639";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void beforeInitView() {
         setContentView(R.layout.activity_my);
     }
 
     @Override
-    protected void beforeInitView() {
-
-    }
-
-    @Override
     protected void initView() {
+        adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId(AD_UNIT_ID);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout);
+        layout.addView(adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        adView.loadAd(adRequest);
 
     }
 
@@ -35,24 +43,31 @@ public class MyActivity extends BaseActivity {
     protected void initData() {
 
     }
-
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public void onResume() {
+        super.onResume();
+        if (adView != null) {
+            adView.resume();
         }
-        return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onPause() {
+        if (adView != null) {
+            adView.pause();
+        }
+        super.onPause();
+    }
+
+    /** Called before the activity is destroyed. */
+    @Override
+    public void onDestroy() {
+        // Destroy the AdView.
+        if (adView != null) {
+            adView.destroy();
+        }
+        super.onDestroy();
+    }
+
+
 }
